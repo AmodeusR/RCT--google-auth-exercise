80fece93088e283aff06d72457546809f394e2bf
Ricardo Magalhães <ricardogleiss@hotmail.com>
2022-08-15 17:10:30 -0300

    updated SignIn comp with user config

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,11 +1,34 @@
 import { GoogleLogo } from "phosphor-react";
-import React from "react";
+
+import { GoogleAuthProvider, signInWithPopup, User } from "firebase/auth";
 
 import "./signin.scss";
+import { auth } from "../../services/firebase";
+import { useState } from "react";
 
 const SignIn = () => {
+  const [user, setUser] = useState<User>({} as User);
+
+  const handleGoogleSignIn = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then(response => {
+        setUser(response.user);        
+      }).catch(error => {
+        console.error(error);        
+      })
+  }
+  
   return (
     <div className="container">
+      <div className="user">
+        {user.photoURL &&
+          <img src={user.photoURL} alt="Foto do usuário da Google" />
+        }
+        <h2>{user.displayName}</h2>
+        <small>{user.email}</small>
+      </div>
+
       <h1>Acesse sua Conta</h1>
       <span>
         Utilizando autenticação social, por exemplo, autenticação com a Google{" "}
@@ -14,7 +37,7 @@ const SignIn = () => {
         fazer cadastro
       </span>
 
-      <button type="button" className="button">
+      <button type="button" className="button" onClick={handleGoogleSignIn}>
         <GoogleLogo size={28} weight="bold" />
         Entrar com o Google
       </button>
